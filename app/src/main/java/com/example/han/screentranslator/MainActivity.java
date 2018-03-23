79241a954354c7c1d057aafd858e6448ac22e642
jakehyo <jskh1999@gmail.com>
2018-03-23 13:06:49 -0700

    Implemented new google api translate java, in replacement for jafregle. Now we need to think of the logistics of the translator and which methods and variables we're going to use. Will we have to transfer information between actvities and fragments? Do we need a save function? We may never know. How are we going to save the settings?

--- app/src/main/java/com/example/han/screentranslator/MainActivity.java
+++ app/src/main/java/com/example/han/screentranslator/MainActivity.java
@@ -1,14 +1,7 @@
 package com.example.han.screentranslator;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import com.jafregle.Jafregle;
-import com.jafregle.Language;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,27 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Translate().execute("Hello World");
 
-    }
-    class Translate extends AsyncTask<String, Void, String>{
 
-        @Override
-        protected String doInBackground(String... strings) {
-            Jafregle j = new Jafregle(Language.ENGLISH, Language.FRENCH);
-            try {
-                //Log.d("asdfasdfsdfasdfasdf", "onCreate: "+j.translate("Hello World!", Language.ENGLISH, Language.FRENCH));
-                return j.translate(strings[0]);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return "ERROR";
-        }
 
-        @Override
-        protected void onPostExecute(String s) {
-            Log.d("Asdf", "onPostExecute: "+s);
-        }
+
+
+
     }
 
 }
